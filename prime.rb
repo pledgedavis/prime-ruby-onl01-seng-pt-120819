497528445d10c677a909e99b8a44b496ff34ed29
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 22:51:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,13 @@
 # Add  code here!
 
 def prime?(integer)
-
+  
+  if integer == 2 
+      return true
+      
+    else
+      
+      
 a=*(2..integer)
   a.each do |number|
     if integer%number == 0
