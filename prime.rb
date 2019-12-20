e3b06ec0ccaac0be34bc9bb0ab17d00525f06eae
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 22:59:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+
+def prime?(integer)
+  
+  if integer == 2 
+      return true
+      elsif   integer < 2   
+      return false 
+      
+     else
+        
+        
+      a=*(2..integer-1)
+        a.each do |number|
+          
+          if integer%number == 0
+            return false
+    
+        end
+      end
+  end
+      true
+end
+
